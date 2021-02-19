9cf7cbd9527b8660ae27ba73f9d8b4638efae4d6
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-02-19 01:16:25 +0100

    Weekly update

--- dataSet.js
+++ dataSet.js
@@ -1,2 +1,2 @@
-const time = 'Feb 13, 2021 21:00:00'; //15:00:00 for retained
-const image = './Archival/zootopia.png';
+const time = 'Feb 19, 2021 15:00:00'; //15:00:00 for retained
+const image = './Archival/up.jpg';
