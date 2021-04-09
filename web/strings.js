5698b1a49a7e4647739be59b25d2acfb5411fa37
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-04-09 02:29:06 +0200

    web backend thing

---
+++ web/strings.js
@@ -0,0 +1,31 @@
+const strings = {
+    time: 'Apr 9, 2021 01:34:00', //15:00:00 for Retained
+    movie: 'the Emoji Movie',
+    imageURL: 'https://raw.githubusercontent.com/MaxTechnics/CoolMovieNightThing/master/Archival/LWA.jpg',
+    opacity: '0.4',
+    postCounterOpacity: '0.2',
+    centerMessage: 'Movie Night starts in:',
+    postCounterMessage: 'Starting soon...',
+    headerText: 'Today we\'re watching: ',
+    footerText: 'Presented by Retainededge and Grady\'s Physics Homework',
+    RPCCountMessage: 'Starts in:',
+    RPCAlmostThere: 'Starts soon!',
+    RPCPostCountMsg: 'Now Playing!',
+    RPClarge_image: 'frontrow',
+    RPCsmall_image: 'homework',
+    RPClarge_text: 'Take a seat',
+    RPCsmall_text: 'This is Maxim',
+    RPCbtn1TXT: 'Movie Info',
+    RPCbtn1URL: 'https://www.imdb.com/title/tt4877122/', // IMDB Link
+    RPCbtn2TXT: 'GitHub',
+    RPCbtn2URL: 'https://github.com/TeraBaito',
+    RPCPostCountlarge_image: 'frontrow',
+    RPCPostCountsmall_image: 'homework',
+    RPCPostCountlarge_text: 'Got popcorn?',
+    RPCPostCountsmall_text: 'This is Maxim',
+    RPCPostCountbtn1TXT: 'Movie Info',
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt4877122/', // IMDB Link
+    RPCPostCountbtn2TXT: 'GitHub',
+    RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
+    latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
+}
