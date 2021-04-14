cfe33a06e67f6730ed387f5cf67dbf6db471515a
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-04-15 01:22:36 +0200

    First actual movie night in forever

--- docs/strings.js
+++ docs/strings.js
@@ -1,6 +1,6 @@
 const strings = {
-    time: 'Apr 10, 2021 22:00:00', //15:00:00 for Retained
-    movie: 'Office Space',
+    time: 'Apr 16, 2021 15:00:00', //15:00:00 for Retained
+    movie: 'Godzilla vs Kong',
     imageURL: 'https://cdn.discordapp.com/attachments/719156784492838973/830442161932992512/unknown.png', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
@@ -12,19 +12,19 @@ const strings = {
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'homework',
+    RPCsmall_image: 'retained',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'This is Maxim',
+    RPCsmall_text: 'Hosted by Retained',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt4877122/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt5034838/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'homework',
+    RPCPostCountsmall_image: 'retained',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'This is Maxim',
+    RPCPostCountsmall_text: 'Hosted by Retained',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt4877122/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt5034838/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
