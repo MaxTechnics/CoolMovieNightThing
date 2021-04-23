4b60969d5a2ce288e0e6b8e90209a499ce55a5a1
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-04-23 20:02:03 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,5 +1,5 @@
 const strings = {
-    time: 'Apr 23, 2021 15:00:00', //15:00:00 for Retained
+    time: 'Apr 23, 2021 21:00:00', //15:00:00 for Retained
     movie: 'Spirited Away',
     imageURL: 'https://news.cgtn.com/news/3d3d514f3363544d35457a6333566d54/img/c66f29f6e575486aa40db56441fa503d/c66f29f6e575486aa40db56441fa503d.jpg', // Cleared or any valid URL
     opacity: '0.4',
@@ -12,17 +12,17 @@ const strings = {
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'retained',
+    RPCsmall_image: 'homework',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Retainededge',
+    RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
     RPCbtn1URL: 'https://www.imdb.com/title/tt0245429/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'retained',
+    RPCPostCountsmall_image: 'homework',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Retainededge',
+    RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
     RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0245429/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
