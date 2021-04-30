945ae03bd8f8df1892365183be88f77fae59b3d9
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-04-30 21:35:03 +0200

    I hate everyone

--- docs/strings.js
+++ docs/strings.js
@@ -1,5 +1,5 @@
 const strings = {
-    time: 'Apr 30, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3
+    time: 'Apr 30, 2021 14:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
     movie: 'Pokemon: The First Movie',
     imageURL: 'https://i0.wp.com/mynintendonews.com/wp-content/uploads/2020/08/pokemon_the_first_movie.jpg', // Cleared or any valid URL
     opacity: '0.4',
@@ -7,22 +7,22 @@ const strings = {
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Facad3, Retainededge and Grady\'s Physics Homework',
+    footerText: 'Presented by Facad3, Retainededge and Tera',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'homework',
+    RPCsmall_image: 'tewwa',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Maxim',
+    RPCsmall_text: 'Hosted by Tera',
     RPCbtn1TXT: 'Movie Info',
     RPCbtn1URL: 'https://www.imdb.com/title/tt0190641/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'homework',
+    RPCPostCountsmall_image: 'tewwa',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Maxim',
+    RPCPostCountsmall_text: 'Hosted by Tera',
     RPCPostCountbtn1TXT: 'Movie Info',
     RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0190641/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
