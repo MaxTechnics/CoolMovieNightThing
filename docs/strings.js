694d7c4620000d299e97e11bd8d7ea18744087af
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-05-07 20:40:19 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,30 +1,30 @@
 const strings = {
-    time: 'Apr 30, 2021 14:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Pokemon: The First Movie',
-    imageURL: 'https://i0.wp.com/mynintendonews.com/wp-content/uploads/2020/08/pokemon_the_first_movie.jpg', // Cleared or any valid URL
+    time: 'May 07, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Megamind',
+    imageURL: 'https://wallpaperaccess.com/full/2101670.jpg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Facad3, Retainededge and Tera',
+    footerText: 'Presented by Grady\'s Physic Homework',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'tewwa',
+    RPCsmall_image: 'homework',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Tera',
+    RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt0190641/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt1001526/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'tewwa',
+    RPCPostCountsmall_image: 'homework',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Tera',
+    RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0190641/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt1001526/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
