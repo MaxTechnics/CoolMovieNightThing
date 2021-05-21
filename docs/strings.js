385b58e5e87600e6f690002cf06870db30a626cb
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-05-21 21:10:41 +0200

    Wall-E

--- docs/strings.js
+++ docs/strings.js
@@ -1,7 +1,7 @@
 const strings = {
-    time: 'May 14, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Age of Ultron',
-    imageURL: 'https://cdn.britannica.com/60/182360-050-CD8878D6/Avengers-Age-of-Ultron-Joss-Whedon.jpg', // Cleared or any valid URL
+    time: 'May 21, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Wall-E',
+    imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
@@ -16,7 +16,7 @@ const strings = {
     RPClarge_text: 'Take a seat',
     RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt2395427/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
@@ -24,7 +24,7 @@ const strings = {
     RPCPostCountlarge_text: 'Got popcorn?',
     RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt2395427/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
