a2d0b3c947e2dba01f35a0b65a2a3bad58f31a5b
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-05-28 21:27:19 +0200

    Weekly update

--- docs/strings.js
+++ docs/strings.js
@@ -1,7 +1,7 @@
 const strings = {
-    time: 'May 21, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Wall-E',
-    imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
+    time: 'May 28, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Detective Pikachu',
+    imageURL: 'https://consequence.net/wp-content/uploads/2019/05/pikachu-e1557247424342.jpg?quality=80', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
