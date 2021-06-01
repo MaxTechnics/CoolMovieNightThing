c28d384138c51e91cec4d2d6d859f0dd8c63df1b
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-01 22:05:23 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,13 +1,13 @@
 const strings = {
     time: 'May 28, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Detective Pikachu',
-    imageURL: 'https://consequence.net/wp-content/uploads/2019/05/pikachu-e1557247424342.jpg?quality=80', // Cleared or any valid URL
+    movie: 'Wall-E',
+    imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
-    headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Grady\'s Physic Homework',
+    headerText: 'Caffeine Church Special!',
+    footerText: 'Presented by Grady\'s Physics Homework',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
