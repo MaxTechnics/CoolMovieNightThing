a6a3b46388afc2db8a60b1a253340ced25df0e55
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-03 21:40:16 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,28 +1,28 @@
 const strings = {
-    time: 'June 1, 2021 22:10:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Wall-E',
-    imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
+    time: 'June 4, 2021 12:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Spiderman Into the Spiderverse and Wall-E',
+    imageURL: '', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
-    headerText: 'Caffeine Church Special!, watching: ',
-    footerText: 'Presented by Grady\'s Physics Homework',
+    headerText: 'Today we\'re watching: ',
+    footerText: 'Presented by Kirito, Retainededge and Grady\'s Physics Homework',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'homework',
+    RPCsmall_image: 'kirito',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Maxim',
+    RPCsmall_text: 'Hosted by Kirito',
     RPCbtn1TXT: 'Movie Info',
     RPCbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'homework',
+    RPCPostCountsmall_image: 'kirito',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Maxim',
+    RPCPostCountsmall_text: 'Hosted by Kirito',
     RPCPostCountbtn1TXT: 'Movie Info',
     RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
