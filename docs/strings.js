a866e48a7a75398be87ceea0a2538ddc8aeb116a
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-04 09:38:17 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,5 +1,5 @@
 const strings = {
-    time: 'June 4, 2021 12:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    time: 'June 4, 2021 15:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
     movie: 'Spiderman Into the Spiderverse and Wall-E',
     imageURL: '', // Cleared or any valid URL
     opacity: '0.4',
@@ -7,22 +7,22 @@ const strings = {
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Kirito, Retainededge and Grady\'s Physics Homework',
+    footerText: 'Presented by Retainededge and Grady\'s Physics Homework',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'kirito',
+    RPCsmall_image: 'retained',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Kirito',
+    RPCsmall_text: 'Hosted by Retained',
     RPCbtn1TXT: 'Movie Info',
     RPCbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'kirito',
+    RPCPostCountsmall_image: 'retained',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Kirito',
+    RPCPostCountsmall_text: 'Hosted by Retained',
     RPCPostCountbtn1TXT: 'Movie Info',
     RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
