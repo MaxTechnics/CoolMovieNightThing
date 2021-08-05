bf21aee6e6448d0075c97c7b2d87f05361be18e1
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-06 00:38:53 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,7 +1,7 @@
 const strings = {
-    time: 'July 16, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Mr. Peabody and Sherman',
-    imageURL: 'https://www.skwigly.co.uk/wp-content/uploads/2014/02/MPS_sq1100_s4_f143_4k_PS_RGB_v1.3.jpg', // Cleared or any valid URL
+    time: 'August 7, 2021 10:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Back to the Future',
+    imageURL: 'https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTGk6epvO6CK5E5Gkwg7PhLJHAXloAi5vLrjrVTf5XJh32cIYQq_HLDbhKPx_TpAGQ4yJ7aX-z9AhnEQzmSK8JFE47G8.jpg?r=bd0', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
@@ -12,19 +12,19 @@ const strings = {
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'homework',
+    RPCsmall_image: 'retained',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Maxim',
+    RPCsmall_text: 'Hosted by Retained',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt0864835/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt0088763/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'homework',
+    RPCPostCountsmall_image: 'retained',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Maxim',
+    RPCPostCountsmall_text: 'Hosted by Retained',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0864835/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0088763/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
