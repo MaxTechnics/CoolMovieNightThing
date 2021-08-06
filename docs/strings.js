c573a92a9f302504e60364c18e36b57ff9e8b83b
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-06 19:44:38 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,5 +1,5 @@
 const strings = {
-    time: 'August 7, 2021 10:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    time: 'August 7, 2021 20:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
     movie: 'Back to the Future',
     imageURL: 'https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTGk6epvO6CK5E5Gkwg7PhLJHAXloAi5vLrjrVTf5XJh32cIYQq_HLDbhKPx_TpAGQ4yJ7aX-z9AhnEQzmSK8JFE47G8.jpg?r=bd0', // Cleared or any valid URL
     opacity: '0.4',
@@ -12,17 +12,17 @@ const strings = {
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'retained',
+    RPCsmall_image: 'homework',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Retained',
+    RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
     RPCbtn1URL: 'https://www.imdb.com/title/tt0088763/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'retained',
+    RPCPostCountsmall_image: 'homework',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Retained',
+    RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
     RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0088763/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
