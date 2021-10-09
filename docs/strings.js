b6d2da7679bd2880fa7a3162364e1b64d43f2f01
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-10-09 15:37:05 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,30 +1,30 @@
 const strings = {
-    time: 'August 6, 2021 20:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Back to the Future',
-    imageURL: 'https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTGk6epvO6CK5E5Gkwg7PhLJHAXloAi5vLrjrVTf5XJh32cIYQq_HLDbhKPx_TpAGQ4yJ7aX-z9AhnEQzmSK8JFE47G8.jpg?r=bd0', // Cleared or any valid URL
+    time: 'October 9, 2021 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Squid Game',
+    imageURL: 'https://media.glamour.com/photos/615b50b9b73cf95da55eea61/64:25/w_5888,h_2300,c_limit/SquidGame_Unit_108_0070.jpg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Retainededge and Grady\'s Physics Homework',
+    footerText: 'Presented by Acama',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'homework',
+    RPCsmall_image: 'frontrow',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Maxim',
+    RPCsmall_text: 'Hosted by Acama',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt0088763/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt10919420/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'homework',
+    RPCPostCountsmall_image: 'frontrow',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Maxim',
+    RPCPostCountsmall_text: 'Hosted by Acama',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0088763/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt10919420/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
