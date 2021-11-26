1f40e6c1e73daafe5df198f098befd114bdd5552
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-11-26 10:10:29 +0100

    I want to use this again

--- docs/strings.js
+++ docs/strings.js
@@ -1,30 +1,30 @@
 const strings = {
-    time: 'October 10, 2021 19:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Squid Game',
-    imageURL: 'https://media.glamour.com/photos/615b50b9b73cf95da55eea61/64:25/w_5888,h_2300,c_limit/SquidGame_Unit_108_0070.jpg', // Cleared or any valid URL
+    time: 'November 26, 2021 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Wall-E',
+    imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Acama',
+    footerText: 'Presented by Grady\'s Physics Homework',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
     RPClarge_image: 'frontrow',
-    RPCsmall_image: 'frontrow',
+    RPCsmall_image: 'homework',
     RPClarge_text: 'Take a seat',
-    RPCsmall_text: 'Hosted by Acama',
+    RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt10919420/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
-    RPCPostCountsmall_image: 'frontrow',
+    RPCPostCountsmall_image: 'homework',
     RPCPostCountlarge_text: 'Got popcorn?',
-    RPCPostCountsmall_text: 'Hosted by Acama',
+    RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt10919420/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
