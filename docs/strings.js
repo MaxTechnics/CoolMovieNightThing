8141bfb79d36a381303ff132def947866411845f
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-11-27 18:51:18 +0100

    Wall e shin

--- docs/strings.js
+++ docs/strings.js
@@ -1,10 +1,10 @@
 const strings = {
-    time: 'November 26, 2021 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    time: 'November 27, 2021 23:30:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
     movie: 'Wall-E',
     imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
-    centerMessage: 'Movie Night starts in:',
+    centerMessage: 'Cute robot and oval grill:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
     footerText: 'Presented by Grady\'s Physics Homework',
