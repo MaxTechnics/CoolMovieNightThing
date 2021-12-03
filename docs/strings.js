9970923ab5115c6a23d4b54c483ce47c67a5961d
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-12-03 20:28:41 +0100

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,10 +1,10 @@
 const strings = {
-    time: 'November 27, 2021 23:30:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Wall-E',
-    imageURL: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/52FA639B55389BFF0482A24E086E3A5381BAD467E426B2A1F49A9F4C02D17FD3/scale?width=2880&aspectRatio=1.78&format=jpeg', // Cleared or any valid URL
+    time: 'December 3, 2021 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Married with... Children',
+    imageURL: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2017%2F06%2Ftcdmawi_ec029_h-2000.jpg&q=85', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
-    centerMessage: 'Cute robot and oval grill:',
+    centerMessage: 'Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
     headerText: 'Today we\'re watching: ',
     footerText: 'Presented by Grady\'s Physics Homework',
@@ -16,7 +16,7 @@ const strings = {
     RPClarge_text: 'Take a seat',
     RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt0092400/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
@@ -24,7 +24,7 @@ const strings = {
     RPCPostCountlarge_text: 'Got popcorn?',
     RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0910970/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0092400/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
