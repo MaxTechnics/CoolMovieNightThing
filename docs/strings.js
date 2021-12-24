44fbf1d645bff3ab64c6efcb719c8db52877cb62
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-12-24 20:18:49 +0100

    The 24h movie night!!
    LETS GOOOOO

--- docs/strings.js
+++ docs/strings.js
@@ -1,13 +1,13 @@
 const strings = {
-    time: 'December 3, 2021 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Married with... Children',
+    time: 'December 24, 2021 22:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Home Alone',
     imageURL: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2017%2F06%2Ftcdmawi_ec029_h-2000.jpg&q=85', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
-    centerMessage: 'Movie Night starts in:',
+    centerMessage: 'The 24 hour Movie Night starts in:',
     postCounterMessage: 'Starting soon...',
-    headerText: 'Today we\'re watching: ',
-    footerText: 'Presented by Grady\'s Physics Homework',
+    headerText: 'We\'re starting with: ',
+    footerText: 'Presented by Grady\'s Physics Homework, thanks for stopping by!',
     RPCCountMessage: 'Starts in:',
     RPCAlmostThere: 'Starts soon!',
     RPCPostCountMsg: 'Now Playing!',
@@ -15,16 +15,16 @@ const strings = {
     RPCsmall_image: 'homework',
     RPClarge_text: 'Take a seat',
     RPCsmall_text: 'Hosted by Maxim',
-    RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt0092400/', // IMDB Link
+    RPCbtn1TXT: 'Movie Invite',
+    RPCbtn1URL: 'https://discord.gg/cXpvUUQe?event=923223839977140265', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
     RPCPostCountsmall_image: 'homework',
     RPCPostCountlarge_text: 'Got popcorn?',
     RPCPostCountsmall_text: 'Hosted by Maxim',
-    RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0092400/', // IMDB Link
+    RPCPostCountbtn1TXT: 'Movie Invite',
+    RPCPostCountbtn1URL: 'https://discord.gg/cXpvUUQe?event=923223839977140265', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
