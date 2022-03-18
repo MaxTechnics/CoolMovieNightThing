a6a3621aaa990efddaed0d5ec92d4b98b36548eb
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2022-03-18 20:28:01 +0100

    A whisker away

--- docs/strings.js
+++ docs/strings.js
@@ -1,7 +1,7 @@
 const strings = {
-    time: 'February 4, 2022 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Spies in Disguise',
-    imageURL: 'https://cdn.mos.cms.futurecdn.net/Bc6Ny5Dec26JvooP5DYLLQ-1200-80.jpg', // Cleared or any valid URL
+    time: 'March 18 2022 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'A Whisker Away',
+    imageURL: 'https://i0.wp.com/omnitos.com/wp-content/uploads/2021/04/feat-4.jpg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
@@ -16,7 +16,7 @@ const strings = {
     RPClarge_text: 'Take a seat',
     RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt5814534/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt11958344/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
@@ -24,7 +24,7 @@ const strings = {
     RPCPostCountlarge_text: 'Got popcorn?',
     RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt5814534/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt11958344/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
