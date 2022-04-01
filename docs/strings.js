a8be686f9f174498c7dad7c87079e1cb52302637
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2022-04-01 20:28:32 +0200

    Update strings.js

--- docs/strings.js
+++ docs/strings.js
@@ -1,7 +1,7 @@
 const strings = {
-    time: 'March 18 2022 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'A Whisker Away',
-    imageURL: 'https://i0.wp.com/omnitos.com/wp-content/uploads/2021/04/feat-4.jpg', // Cleared or any valid URL
+    time: 'April 1 2022 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Castle In The Sky',
+    imageURL: 'https://m.media-amazon.com/images/M/MV5BMjAxOTkxMzU3MF5BMl5BanBnXkFtZTcwNzI4ODAyNw@@._V1_.jpg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
@@ -16,7 +16,7 @@ const strings = {
     RPClarge_text: 'Take a seat',
     RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt11958344/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt0092067/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
@@ -24,8 +24,8 @@ const strings = {
     RPCPostCountlarge_text: 'Got popcorn?',
     RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt11958344/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0092067/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
-    RPCPostCountbtn2URL: 'https://github.com/TeraBaito',
+    RPCPostCountbtn2URL: 'https://github.com/MaxTechnics/CoolMovieNightThing',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
 }
