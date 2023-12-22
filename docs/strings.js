0da135eddf0444c175e1b28dcd4af9871a5a6439
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2023-12-22 19:52:09 +0100

    Big hero 6

--- docs/strings.js
+++ docs/strings.js
@@ -1,7 +1,7 @@
 const strings = {
-    time: 'April 1 2022 21:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
-    movie: 'Castle In The Sky',
-    imageURL: 'https://m.media-amazon.com/images/M/MV5BMjAxOTkxMzU3MF5BMl5BanBnXkFtZTcwNzI4ODAyNw@@._V1_.jpg', // Cleared or any valid URL
+    time: 'December 22 2023 20:00:00', //15:00:00 for Retained | 22:00:00 for yours truly | 04:00:00 for Facad3 | 14:00:00 for tewwa
+    movie: 'Big Hero 6',
+    imageURL: 'https://images8.alphacoders.com/605/605895.jpg', // Cleared or any valid URL
     opacity: '0.4',
     postCounterOpacity: '0.2',
     centerMessage: 'Movie Night starts in:',
@@ -16,7 +16,7 @@ const strings = {
     RPClarge_text: 'Take a seat',
     RPCsmall_text: 'Hosted by Maxim',
     RPCbtn1TXT: 'Movie Info',
-    RPCbtn1URL: 'https://www.imdb.com/title/tt0092067/', // IMDB Link
+    RPCbtn1URL: 'https://www.imdb.com/title/tt2245084/', // IMDB Link
     RPCbtn2TXT: 'Great game while you wait',
     RPCbtn2URL: 'osu://',
     RPCPostCountlarge_image: 'frontrow',
@@ -24,7 +24,7 @@ const strings = {
     RPCPostCountlarge_text: 'Got popcorn?',
     RPCPostCountsmall_text: 'Hosted by Maxim',
     RPCPostCountbtn1TXT: 'Movie Info',
-    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt0092067/', // IMDB Link
+    RPCPostCountbtn1URL: 'https://www.imdb.com/title/tt2245084/', // IMDB Link
     RPCPostCountbtn2TXT: 'GitHub',
     RPCPostCountbtn2URL: 'https://github.com/MaxTechnics/CoolMovieNightThing',
     latestVersion: 1.1 // has to be a digit, symantic versioning beyond x.x (like x.x.x) is not supported
